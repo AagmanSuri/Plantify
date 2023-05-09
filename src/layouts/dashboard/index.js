5ccbfc03afec7847ca13f8713fc2495e85454ec6
amey-323 <ameygangan19@gmail.com>
2023-05-09 07:51:15 +0530

    integrated model with dashboard

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -2,6 +2,13 @@ import { useState, useEffect } from "react";
 
 // @mui material components
 import Grid from "@mui/material/Grid";
+import Form from "react-bootstrap/Form";
+import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.css";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
@@ -24,6 +31,66 @@ import { useLocation, NavLink } from "react-router-dom";
 function Dashboard() {
   const [db, setDb] = useState([]);
   const [localUpdateTime, setlocalUpdateTime] = useState("");
+  const [file, setFile] = useState();
+  const [result, setResult] = useState("");
+  const [hasImage, setHasImage] = useState(false);
+  const [fileUrl, setFileUrl] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
+
+  //Code for disease detection
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     isLoading: false,
+  //     hasImage: false,
+  //     imageUrl: "",
+  //     fileUrl: "",
+  //     file: null,
+  //     result: "",
+  //   };
+  // }
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    console.log(value + " AND " + name);
+    console.log(event.target.files[0]);
+    //var hasImage = hasImage;
+    setHasImage(value !== "");
+    setFileUrl(...fileUrl, URL.createObjectURL(event.target.files[0]));
+    setFile(event.target.files[0]);
+  };
+
+  const handlePredictClick = (event) => {
+    //const file = file;
+    //setFile(file,file);
+    console.log(file);
+    const data = new FormData();
+    console.log(data);
+    data.append("file", file);
+    setIsLoading(true);
+
+    fetch("http://127.0.0.1:5000/prediction/", {
+      //mode: "no-cors",
+      method: "POST",
+      body: data,
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setResult(response.result);
+        setIsLoading(false);
+      });
+  };
+
+  const handleCancelClick = (event) => {
+    setFile(null);
+    setResult("");
+    setHasImage(false);
+    setFileUrl("");
+  };
 
   const getDb = async () => {
     const response = await fetch(
@@ -188,13 +255,67 @@ function Dashboard() {
           </Grid>
         </MDBox>
         <MDBox>
-          <Grid container spacing={3}>
-            <Grid item xs={12} md={6} lg={8}>
-              {/* <Projects /> */}
-            </Grid>
-            <Grid item xs={12} md={6} lg={4}>
-              {/* <OrdersOverview /> */}
-            </Grid>
+          <Grid container spacing={0}>
+            {/* <Grid item xs={12} md={6} lg={8}> */}
+            {/* <Projects /> */}
+            <Form>
+              <Row>
+                <Form.Group as={Col}>
+                  <Form.Label>Upload Image</Form.Label>
+                  <Form.Control
+                    type="file"
+                    //   placeholder="Text Field 1"
+
+                    name="image"
+                    value={imageUrl}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+              </Row>
+              <Row>
+                {hasImage ? (
+                  <Col>
+                    <Image
+                      height={300}
+                      width={300}
+                      src={fileUrl}
+                      roundedCircle
+                    />
+                  </Col>
+                ) : null}
+              </Row>
+
+              <Row>
+                <Col>
+                  <Button
+                    block
+                    variant="success"
+                    disabled={isLoading}
+                    onClick={!isLoading ? handlePredictClick : null}
+                  >
+                    {isLoading ? "Making prediction" : "Predict"}
+                  </Button>
+                </Col>
+                <Col>
+                  <Button
+                    block
+                    variant="danger"
+                    disabled={isLoading}
+                    onClick={handleCancelClick}
+                  >
+                    Reset prediction
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+            {result === "" ? null : (
+              <Row>
+                <Col className="result-container">
+                  <h5 id="result">{result}</h5>
+                </Col>
+              </Row>
+            )}
+            {/* </Grid> */}
           </Grid>
         </MDBox>
       </MDBox>
